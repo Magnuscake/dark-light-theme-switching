77c45d196b9d42f07b64664420889bf15223ac47
Magnuscake <saneyar.k@gmail.com>
2020-10-27 21:34:16 +1100

    Code cleanup and aesthetic changes

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
+const DARK_MODE = "dark";
+const LIGHT_MODE = "light";
+
 // Switch between light or dark images
 function switchIllustrations(mode) {
   image1.src = `assets/undraw_Content_${mode}.svg`;
@@ -17,8 +20,12 @@ function toggleDarkLightMode(isDark) {
   nav.style.backgroundColor = isDark
     ? "rgb(0 0 0/ 50%)"
     : "rgb(255 255 255 / 50%)";
-  textBox.style.color = isDark ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = isDark
+    ? "rgb(255 255 255 / 50%)"
+    : "rgb(0 0 0 / 50%)";
+
   toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+
   isDark
     ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
     : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
@@ -27,12 +34,12 @@ function toggleDarkLightMode(isDark) {
 
 function switchTheme(e) {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", DARK_MODE);
+    localStorage.setItem("theme", DARK_MODE);
     toggleDarkLightMode(true);
   } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", LIGHT_MODE);
+    localStorage.setItem("theme", LIGHT_MODE);
     toggleDarkLightMode(false);
   }
 }
@@ -44,7 +51,7 @@ const currentTheme = localStorage.getItem("theme");
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme === "dark") {
+  if (currentTheme === DARK_MODE) {
     toggleSwitch.checked = true;
     toggleDarkLightMode(true);
   }
